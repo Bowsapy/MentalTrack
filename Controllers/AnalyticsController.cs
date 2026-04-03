cc0b0aa31f101ff960412318817586cff08c0ac3
Bowsapy <janbouza5@seznam.cz>
2026-04-03 17:41:45 +0200

    Dodelani UserStates: Formular na webu + propsani do databaze

--- Controllers/AnalyticsController.cs
+++ Controllers/AnalyticsController.cs
@@ -13,9 +13,10 @@ namespace MentalTrack.Controllers
         private readonly AppDbContext _context;
         private readonly EmbeddingService _embeddingService;
 
-        public AnalyticsController(AppDbContext context)
+        public AnalyticsController(AppDbContext context, EmbeddingService embeddingService)
         {
             _context = context;
+            _embeddingService = embeddingService;
         }
         public IActionResult Index()
         {
@@ -25,9 +26,14 @@ namespace MentalTrack.Controllers
         {
             return View();
         }
-        public async void CreateNewUserState(string content, UserStateEnum state)
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewUserState(string content, UserStateEnum state)
         {
             _context.UserStates.Add(new UserStatesEmb(state, content, await _embeddingService.GetEmbedding(content)));
+            _context.SaveChanges();
+            return RedirectToAction("AddNewUserState");
+
         }
     }
 }
---
+++ ViewModels/CreateNewUserStateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MentalTrack.Enums;
+
+namespace MentalTrack.ViewModels
+{
+    public class CreateNewUserStateViewModel
+    {
+        public string Content { get; set; }
+        public UserStateEnum State { get; set; }
+
+    }
+}
