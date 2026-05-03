81c1d46c503133916359cf745f5baabaa61914fe
Bowsapy <janbouza5@seznam.cz>
2026-05-03 09:51:18 +0200

    Uprava displaystate UserState enumu ve ViewMatches.cshtml

--- Enums/UserStateEnum.cs
+++ Enums/UserStateEnum.cs
@@ -1,11 +1,22 @@
-﻿namespace MentalTrack.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MentalTrack.Enums
 {
     public enum UserStateEnum
     {
+        [Display (Name = "Being anxious")]
         Anxious = 0,
-        Depressed=1,
-        Tired=2,
+        [Display(Name = "Being depressed")]
+
+        Depressed = 1,
+        [Display(Name = "Being tired")]
+
+        Tired = 2,
+        [Display(Name = "Work")]
+
         Working = 3,
+        [Display(Name = "Being alone")]
+
         Alone = 4,
     }
 }
