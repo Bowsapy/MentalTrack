add03fc942c54525e4445283d0a4a1c11e3db487
Bowsapy <janbouza5@seznam.cz>
2026-07-11 16:39:54 +0200

    Přendal jsem většinu metod z AnalyticsControleru do StatisticsService

--- Controllers/AnalyticsController.cs
+++ Controllers/AnalyticsController.cs
@@ -77,247 +77,45 @@ namespace MentalTrack.Controllers
         return userId;
         }
 
-        public void FindEntryStateMatches()
-        {
-
-
-            //ziskej journal entry parts konkretniho usera jako list
-            var entryParts = _context.EntryParts
-                .Where(e => e.Embedding != null
-                    && e.JournalEntry.UserId == GetCurrentUser()).Include(x => x.Sentiment)
-                .ToList();
-
-
-            //ziskej userstates jako list
-            var allUserStates = _context.UserStates
-                .Where(s => s.Embedding != null).Include(x => x.Sentiment)
-                .ToList();
-
-            var existingMatches = _context.EntryStates
-      .Where(x => x.JournalEntry.UserId == GetCurrentUser())
-      .Select(x => new { x.JournalEntryId, x.UserStatesEmbId })
-      .AsEnumerable()
-      .Select(x => (x.JournalEntryId, x.UserStatesEmbId))
-      .ToHashSet();
-
-    //vytvorim hash set pro zjisteni poctu, protoze list by sezral moc pameti
-
-            var matches = new List<EntryStateScore>();
-
-            foreach (var part in entryParts)
-            {
-                var entryVector = _embeddingConverter.ConvertToFloatList(part.Embedding);
-
-                Sentiment Part_sentiment = part.Sentiment;
-                var bestMatches = allUserStates.Where(x => x.Sentiment.MainPolarity == Part_sentiment.MainPolarity)
-                    .Select(state => new
-                    {
-                        state.Id,
-                        Score = _similarityService.Calculate(
-                            entryVector,
-                            _embeddingConverter.ConvertToFloatList(state.Embedding))
-                    })
-                    .OrderByDescending(x => x.Score)
-                    .Take(5); 
-                //vytvori anonym. strkturu kde je userstate id, skore s entries
-
-
-                foreach (var match in bestMatches)
-                {
-                    if (match.Score > AppConstants.MinScore)
-                    {
-                        if (!existingMatches.Contains((part.JournalEntryId, match.Id))){ 
-                        
-                            matches.Add(new EntryStateScore(part.JournalEntryId, match.Id, match.Score));                    }
-
-
-                        //pokud dosavadni entrystates neobsahuji tuhle shodu tak ji tam pridej (kontrola starych shod ktery uz tam jsou pridany
-
-                    }
-                }
-            }
-            _context.EntryStates.AddRange(matches);
-            _context.SaveChanges();
-        }
-
-
-        public Dictionary<MoodEnum, Dictionary<UserStateEnum, int>> GetMoodStatesMatches()
-        {
-            FindEntryStateMatches();
-            //zjisti shody a vytvori EntryStates v db (je to hruza, chtelo by to cele prepsat)
-
-            var result = _context.EntryStates
-                .Where(x => x.JournalEntry.UserId == GetCurrentUser())
-                .Select(x => new
-                {
-                    x.JournalEntryId,
-                    Mood = x.JournalEntry.Mood,
-                    UserState = x.UserStatesEmb.UserState
-                })
-                .Distinct()
-                .GroupBy(x => new
-                {
-                    x.Mood,
-                    x.UserState
-                })
-                .Select(g => new
-                {
-                    g.Key.Mood,
-                    g.Key.UserState,
-                    Count = g.Count()
-                })
-                .ToList()
-                .GroupBy(x => x.Mood)
-                .ToDictionary(
-                    g => g.Key,
-                    g => g.ToDictionary(x => x.UserState, x => x.Count)
-                );
-
-            return result;
-        }
-
-        public Dictionary<string,int> ViewMoodOnWeekDays()
-        {
-            var data = _context.Entries.Where(x => x.UserId == GetCurrentUser())
-       .AsEnumerable()
-       .GroupBy(e => e.CreatedAt.DayOfWeek)
-       .ToDictionary(
-           g => g.Key.ToString(),
-           g => (int)g.Average(x => (int)x.Mood)
-       );
-            return data;
-        }
-        public Dictionary<string,int> ViewMoodOnDayPhases()
-        {
-            var data = _context.Entries.Where(x => x.UserId == GetCurrentUser())
-       .AsEnumerable()
-       .GroupBy(e => e.DayPhase)
-       .ToDictionary(
-           g => g.Key.ToString(),
-           g => (int)g.Average(x => (int)x.Mood)
-       );
-            return data;
-        }
+       
+   
 
 
         public IActionResult MoodSummary()
         {
-            Dictionary<MoodEnum,Dictionary<UserStateEnum,int>> USMatches = GetMoodStatesMatches();
-            Dictionary<string, int> WDMatches = ViewMoodOnWeekDays();
-            Dictionary<string, int> DPMatches = ViewMoodOnDayPhases();
+            Dictionary<MoodEnum,Dictionary<UserStateEnum,int>> USMatches = _statisticsService.GetMoodStatesMatches(GetCurrentUser());
+            Dictionary<string, int> WDMatches = _statisticsService.ViewMoodOnWeekDays(GetCurrentUser());
+            Dictionary<string, int> DPMatches = _statisticsService.ViewMoodOnDayPhases(GetCurrentUser());
 
 
-            MoodSummaryViewModel moodsumVM = new MoodSummaryViewModel(USMatches, WDMatches, DPMatches,GetMoodPercentages(),GetAverageModeForAllEntries(),GetEntriesCount());
+            MoodSummaryViewModel moodsumVM = new MoodSummaryViewModel(USMatches, WDMatches, DPMatches,_statisticsService.GetMoodPercentages(),_statisticsService.GetEntriesMode(),_statisticsService.GetEntriesCount());
 
             return View(moodsumVM);
         }
         public IActionResult ShowMoodProgress()
         {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            var entries = _context.Entries
-                .Where(e => e.Mood != null && e.CreatedAt != null && e.UserId == GetCurrentUser())
-                .OrderBy(e => e.CreatedAt)
-                .ToList(); // ToList() vyhodnotí query a pošle jen validní data
-
-            var createdAts = entries
-            .Select(e => e.CreatedAt.ToString("dd.MM.yyyy"))
-            .ToArray();
-
-            var moods = entries
-                .Select(e => e.Mood)
-                .ToArray();
-
-            MoodGraphViewModel mgwm = new MoodGraphViewModel(createdAts, moods);
-            return View(mgwm);
+            return View("ShowMoodProgress", _statisticsService.GetDataForGraphForAllEntries(GetCurrentUser()));
 
         }
 
         public IActionResult ShowMoodProgressMonthly()
         {
 
-            var monthlyData = _context.Entries
-                .Where(e => e.Mood != null && e.UserId == GetCurrentUser())
-                .GroupBy(e => new
-                {
-                    e.CreatedAt.Year,
-                    e.CreatedAt.Month
-                })
-                .OrderBy(g => g.Key.Year)
-                .ThenBy(g => g.Key.Month)
-                .Select(g => new
-                {
-                    Label = $"{g.Key.Month:D2}/{g.Key.Year}",
-                    AverageMood = g.Average(x => (int)x.Mood)
-                })
-                .ToList();
 
-            var createdAts = monthlyData
-                .Select(x => x.Label)
-                .ToArray();
 
-            var moods = monthlyData
-        .Select(x => (MoodEnum)(int)Math.Round(x.AverageMood))
-        .ToArray();
-
-            MoodGraphViewModel mgwm = new MoodGraphViewModel(createdAts, moods, "Your average monthly mood");
-
-            return View("ShowMoodProgress", mgwm);
+            return View("ShowMoodProgress", _statisticsService.GetMonthlyDataForGraph(GetCurrentUser()));
         }
 
         public IActionResult ShowMoodProgressDaily()
         {
 
-            var dailyData = _context.Entries
-                .Where(e => e.Mood != null && e.UserId == GetCurrentUser())
-                .GroupBy(e => new
-                {
-                    e.CreatedAt.Year,
-                    e.CreatedAt.Month,
-                    e.CreatedAt.Day
 
-                })
-                .OrderBy(g => g.Key.Year)
-                .ThenBy(g => g.Key.Month)
-                .ThenBy(g => g.Key.Day)
-
-                .Select(g => new
-                {
-                    Label = $"{g.Key.Day:D2}/{g.Key.Month:D2}/{g.Key.Year}",
-                    AverageMood = g.Average(x => (int)x.Mood)
-                })
-                .ToList();
-
-            var createdAts = dailyData
-                .Select(x => x.Label)
-                .ToArray();
-
-            var moods = dailyData
-        .Select(x => (MoodEnum)(int)Math.Round(x.AverageMood))
-        .ToArray();
-
-            MoodGraphViewModel mgwm = new MoodGraphViewModel(createdAts, moods,"Your average daily mood");
-
-            return View("ShowMoodProgress", mgwm);
+            return View("ShowMoodProgress", _statisticsService.GetDailyDataForGraph(GetCurrentUser()));
         }
-        public int GetAverageModeForAllEntries()
-        {
-            return _statisticsService.GetEntriesMode();
-        }
-        public Dictionary<MoodEnum,double> GetMoodPercentages()
-        {
+ 
 
-            return _context.Entries.GroupBy(x => x.Mood).ToDictionary(x => x.Key, x => Math.Round(((double)x.Count() / GetEntriesCount()) * 100));
-
-
-        }
-        public int GetEntriesCount()
-        {
-
-            int totalCount = _context.Entries.Count();
-            return totalCount;
-
-        }
+ 
 
 
 
--- Services/StatisticsService.cs
+++ Services/StatisticsService.cs
@@ -1,5 +1,13 @@
-﻿using MentalTrack.Data;
+﻿using AspNetCoreGeneratedDocument;
+using Humanizer;
+using MentalTrack.Constants;
+using MentalTrack.Data;
+using MentalTrack.Enums;
 using MentalTrack.Models;
+using MentalTrack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace MentalTrack.Services
 {
@@ -7,11 +15,22 @@ namespace MentalTrack.Services
     {
 
         private readonly AppDbContext _context;
+        private readonly EmbeddingService _embeddingService;
+        private readonly CosineSimilarityService _similarityService;
+        private readonly EmbeddingConverter _embeddingConverter;
 
-        public StatisticsService(AppDbContext context)
+
+
+
+        public StatisticsService(AppDbContext context, EmbeddingConverter embeddingConverter, EmbeddingService embeddingService, CosineSimilarityService similarity)
         {
             _context = context;
+            _similarityService = similarity;
+            _embeddingService = embeddingService;
+            _embeddingConverter = embeddingConverter;
         }
+
+
 
         public int GetEntriesMode()
         {
@@ -19,11 +38,229 @@ namespace MentalTrack.Services
             return mode;
 
         }
+        public int GetEntriesCount()
+        {
+
+            int totalCount = _context.Entries.Count();
+            return totalCount;
+
+        }
+        public Dictionary<MoodEnum, double> GetMoodPercentages()
+        {
+
+            return _context.Entries.GroupBy(x => x.Mood).ToDictionary(x => x.Key, x => Math.Round(((double)x.Count() / GetEntriesCount()) * 100));
 
 
-        
+        }
+        public Dictionary<string, int> ViewMoodOnWeekDays(string CurrentUserId)
+        {
+            var data = _context.Entries.Where(x => x.UserId == CurrentUserId)
+       .AsEnumerable()
+       .GroupBy(e => e.CreatedAt.DayOfWeek)
+       .ToDictionary(
+           g => g.Key.ToString(),
+           g => (int)g.Average(x => (int)x.Mood)
+       );
+            return data;
+        }
+        public Dictionary<string, int> ViewMoodOnDayPhases(string CurrentUserId)
+        {
+            var data = _context.Entries.Where(x => x.UserId == CurrentUserId)
+       .AsEnumerable()
+       .GroupBy(e => e.DayPhase)
+       .ToDictionary(
+           g => g.Key.ToString(),
+           g => (int)g.Average(x => (int)x.Mood)
+       );
+            return data;
+        }
+
+        public void FindEntryStateMatches(string CurrentUserId)
+        {
 
 
+            //ziskej journal entry parts konkretniho usera jako list
+            var entryParts = _context.EntryParts
+                .Where(e => e.Embedding != null
+                    && e.JournalEntry.UserId == CurrentUserId).Include(x => x.Sentiment)
+                .ToList();
 
+
+            //ziskej userstates jako list
+            var allUserStates = _context.UserStates
+                .Where(s => s.Embedding != null).Include(x => x.Sentiment)
+                .ToList();
+
+            var existingMatches = _context.EntryStates
+      .Where(x => x.JournalEntry.UserId == CurrentUserId)
+      .Select(x => new { x.JournalEntryId, x.UserStatesEmbId })
+      .AsEnumerable()
+      .Select(x => (x.JournalEntryId, x.UserStatesEmbId))
+      .ToHashSet();
+
+            //vytvorim hash set pro zjisteni poctu, protoze list by sezral moc pameti
+
+            var matches = new List<EntryStateScore>();
+
+            foreach (var part in entryParts)
+            {
+                var entryVector = _embeddingConverter.ConvertToFloatList(part.Embedding);
+
+                Sentiment Part_sentiment = part.Sentiment;
+                var bestMatches = allUserStates.Where(x => x.Sentiment.MainPolarity == Part_sentiment.MainPolarity)
+                    .Select(state => new
+                    {
+                        state.Id,
+                        Score = _similarityService.Calculate(
+                            entryVector,
+                            _embeddingConverter.ConvertToFloatList(state.Embedding))
+                    })
+                    .OrderByDescending(x => x.Score)
+                    .Take(5);
+                //vytvori anonym. strkturu kde je userstate id, skore s entries
+
+
+                foreach (var match in bestMatches)
+                {
+                    if (match.Score > AppConstants.MinScore)
+                    {
+                        if (!existingMatches.Contains((part.JournalEntryId, match.Id)))
+                        {
+
+                            matches.Add(new EntryStateScore(part.JournalEntryId, match.Id, match.Score));
+                        }
+
+
+                        //pokud dosavadni entrystates neobsahuji tuhle shodu tak ji tam pridej (kontrola starych shod ktery uz tam jsou pridany
+
+                    }
+                }
+            }
+            _context.EntryStates.AddRange(matches);
+            _context.SaveChanges();
+        }
+
+
+        public Dictionary<MoodEnum, Dictionary<UserStateEnum, int>> GetMoodStatesMatches(string CurrentUserId)
+        {
+            FindEntryStateMatches(CurrentUserId);
+            //zjisti shody a vytvori EntryStates v db (je to hruza, chtelo by to cele prepsat)
+
+            var result = _context.EntryStates
+                .Where(x => x.JournalEntry.UserId == CurrentUserId)
+                .Select(x => new
+                {
+                    x.JournalEntryId,
+                    Mood = x.JournalEntry.Mood,
+                    UserState = x.UserStatesEmb.UserState
+                })
+                .Distinct()
+                .GroupBy(x => new
+                {
+                    x.Mood,
+                    x.UserState
+                })
+                .Select(g => new
+                {
+                    g.Key.Mood,
+                    g.Key.UserState,
+                    Count = g.Count()
+                })
+                .ToList()
+                .GroupBy(x => x.Mood)
+                .ToDictionary(
+                    g => g.Key,
+                    g => g.ToDictionary(x => x.UserState, x => x.Count)
+                );
+
+            return result;
+        }
+
+        public MoodGraphViewModel GetMonthlyDataForGraph(string CurrentUserId)
+        {
+            var monthlyData = _context.Entries
+                   .Where(e => e.Mood != null && e.UserId == CurrentUserId)
+                   .GroupBy(e => new
+                   {
+                       e.CreatedAt.Year,
+                       e.CreatedAt.Month
+                   })
+                   .OrderBy(g => g.Key.Year)
+                   .ThenBy(g => g.Key.Month)
+                   .Select(g => new
+                   {
+                       Label = $"{g.Key.Month:D2}/{g.Key.Year}",
+                       AverageMood = g.Average(x => (int)x.Mood)
+                   })
+                   .ToList();
+
+            var createdAts = monthlyData
+                .Select(x => x.Label)
+                .ToArray();
+
+            var moods = monthlyData
+        .Select(x => (MoodEnum)(int)Math.Round(x.AverageMood))
+        .ToArray();
+
+            MoodGraphViewModel mgwm = new MoodGraphViewModel(createdAts, moods, "Your average monthly mood");
+            return mgwm;
+        }
+
+        public MoodGraphViewModel GetDailyDataForGraph(string CurrentUserId)
+
+        {
+            var dailyData = _context.Entries
+            .Where(e => e.Mood != null && e.UserId == CurrentUserId)
+            .GroupBy(e => new
+            {
+                e.CreatedAt.Year,
+                e.CreatedAt.Month,
+                e.CreatedAt.Day
+
+            })
+            .OrderBy(g => g.Key.Year)
+            .ThenBy(g => g.Key.Month)
+            .ThenBy(g => g.Key.Day)
+
+            .Select(g => new
+            {
+                Label = $"{g.Key.Day:D2}/{g.Key.Month:D2}/{g.Key.Year}",
+                AverageMood = g.Average(x => (int)x.Mood)
+            })
+            .ToList();
+
+            var createdAts = dailyData
+                .Select(x => x.Label)
+                .ToArray();
+
+            var moods = dailyData
+        .Select(x => (MoodEnum)(int)Math.Round(x.AverageMood))
+        .ToArray();
+
+            MoodGraphViewModel mgwm = new MoodGraphViewModel(createdAts, moods, "Your average daily mood");
+            return mgwm;
+
+        }
+
+        public MoodGraphViewModel GetDataForGraphForAllEntries(string CurrentUserId)
+        {
+
+            var entries = _context.Entries
+                .Where(e => e.Mood != null && e.CreatedAt != null && e.UserId == CurrentUserId)
+                .OrderBy(e => e.CreatedAt)
+                .ToList(); // ToList() vyhodnotí query a pošle jen validní data
+
+            var createdAts = entries
+            .Select(e => e.CreatedAt.ToString("dd.MM.yyyy"))
+            .ToArray();
+
+            var moods = entries
+                .Select(e => e.Mood)
+                .ToArray();
+
+            MoodGraphViewModel mgwm = new MoodGraphViewModel(createdAts, moods);
+            return mgwm;
+
+        }
     }
 }
