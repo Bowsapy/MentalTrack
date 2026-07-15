d509b77b46b7f903af84ce510e2047db0ed3d661
Bowsapy <janbouza5@seznam.cz>
2026-07-15 20:50:29 +0200

    Design a přidání AboutMe.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace MentalTrack.Controllers
         {
             return View();
         }
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
