9b8114ea48f556e2d130ca6430a5c24e8179ed23
Bowsapy <janbouza5@seznam.cz>
2026-07-23 19:10:17 +0200

    Zmena databaze na postgress na rpi

--- Program.cs
+++ Program.cs
@@ -18,10 +18,10 @@ DotNetEnv.Env.Load();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
-
-
+///fcc
 
 
 
